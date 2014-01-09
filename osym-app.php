626fc37c47cbcf021f61a86d27888e6ad25dd02f
Samet Atdag <samet@Debian-60-squeeze-64-minimal>
2014-01-09 11:16:58 +0100

    this is my new feature.

--- osym-app.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string = file_get_contents("/home/sirin/osym.txt");
-//$string= file_get_contents("/home/sirin/new-osym.txt");
-$json_a=json_decode($string,true);
-$encoded = json_encode($json_a);
-echo $encoded;
-
-?>
